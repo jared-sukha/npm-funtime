17683885ffa6f25635276d59ec4ef49be914548c
jared-sukha <jared.sukha@gmail.com>
2022-05-28 19:48:30 +1200

    typos

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-//Okay, to anyone/tutor?? who is reading this, to get this chalk-animation module to work I had to configure the package.json file to see the project as ES modules, and apparently the way they import files is as below, it was loads of fun trying to get this to work (ie /sarcasm took me far too long)
+//Okay, to anyone/tutor?? who is reading this, to get this chalk-animation module to work I had to configure the package.json file to see the project as ES modules, and apparently the way they import/refer to files (or maybe I should say dependencies instead of files) is as below, it was loads of fun trying to get this to work Lolz(ie took me far too long)
 
 import cowsay from 'cowsay'
-// the 'original' way as below:
-// const cowsay = require('cowsay')
 import chalkAnimation from 'chalk-animation'
 // the 'original' way as below:
+// const cowsay = require('cowsay')
 // const chalkAnimation = require('chalk-animation')
 
 chalkAnimation.rainbow(
   cowsay.say({
-    text: "I'm a fancy dino-module",
+    text: "I'm a slightly fancy dino-module",
     e: 'oO',
     T: 'U ',
     f: 'stegosaurus',
